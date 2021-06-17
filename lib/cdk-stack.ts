07002a2698abec94a4f0ce7f1d9a4c282ab475b8
Claudio Morgia <cmorgia@gmail.com>
2021-06-17 12:38:47 +0200

    Updates

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,9 +1,104 @@
+import { AutoScalingGroup, Signals } from '@aws-cdk/aws-autoscaling';
+import { Certificate } from '@aws-cdk/aws-certificatemanager';
+import { AllowedMethods, Distribution, SecurityPolicyProtocol, SSLMethod, ViewerCertificate, ViewerProtocolPolicy } from '@aws-cdk/aws-cloudfront';
+import { LoadBalancerV2Origin, S3Origin } from '@aws-cdk/aws-cloudfront-origins';
+import { AmazonLinuxImage, CloudFormationInit, InitCommand, InitConfig, InitFile, InitGroup, InitPackage, InitService, InstanceClass, InstanceSize, InstanceType, Port, SubnetType, Vpc } from '@aws-cdk/aws-ec2';
+import { ApplicationLoadBalancer, ApplicationProtocol, ApplicationTargetGroup, TargetType } from '@aws-cdk/aws-elasticloadbalancingv2';
+import { DatabaseClusterEngine, ParameterGroup, ServerlessCluster } from '@aws-cdk/aws-rds';
+import { Bucket } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
+import { Duration } from '@aws-cdk/core';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'vpc', {
+      cidr: '10.0.0.0/24',
+      maxAzs: 3,
+      subnetConfiguration: [
+        { name: 'public', subnetType: SubnetType.PUBLIC },
+        { name: 'private', subnetType: SubnetType.PRIVATE }
+      ]
+    });
+
+    const alb = new ApplicationLoadBalancer(this, 'alb', {
+      vpc: vpc,
+      internetFacing: true
+    });
+
+    const tg = new ApplicationTargetGroup(this, 'tg', {
+      protocol: ApplicationProtocol.HTTP,
+      vpc: vpc,
+      targetType: TargetType.INSTANCE
+    });
+
+    alb.addListener('albListener', { defaultTargetGroups: [tg], protocol: ApplicationProtocol.HTTP });
+
+    const asg = new AutoScalingGroup(this, 'asg', {
+      vpc: vpc,
+      instanceType: InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO),
+      machineImage: new AmazonLinuxImage(),
+      maxCapacity: 8,
+      minCapacity: 0,
+      maxInstanceLifetime: Duration.days(10),
+      spotPrice: '0.5',
+      init: CloudFormationInit.fromConfigSets({
+        configSets: {
+          default: ['yumPreinstall', 'config'],
+        },
+        configs: {
+          yumPreinstall: new InitConfig([
+            InitPackage.yum('httpd24'),
+          ]),
+          config: new InitConfig([
+            InitFile.fromAsset('/var/www/html/demo.html','lib/demo.html'),
+            InitFile.fromAsset('/tmp/perms.sh','lib/perms.sh'),
+            InitGroup.fromName('www'),
+            InitService.enable('httpd'),
+            InitCommand.shellCommand('sh -c /tmp/perms.sh')
+          ]),
+        },
+      }),
+      signals: Signals.waitForAll({
+        timeout: Duration.minutes(10),
+      })
+    });
+
+    asg.attachToApplicationTargetGroup(tg);
+
+    asg.connections.allowFrom(alb, Port.tcp(80));
+    asg.scaleOnCpuUtilization('cpuScale', { targetUtilizationPercent: 50 });
+
+    const staticFiles = new Bucket(this,'staticFiles',{
+      websiteIndexDocument: 'demo.html'
+    });
+
+    const certificate = new Certificate(this, 'Certificate', {
+      domainName: 'example.com',
+      subjectAlternativeNames: ['*.example.com'],
+    });
+
+    const myWebDistribution = new Distribution(this, 'myDist', {
+      defaultBehavior: {
+        origin: new LoadBalancerV2Origin(alb),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      additionalBehaviors: {
+        'static/*': {
+          origin: new S3Origin(staticFiles),
+          viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        }
+      },
+      certificate: certificate,
+      domainNames: ['example.com', 'www.example.com']
+    });
+
+    const cluster = new ServerlessCluster(this, 'auroraServerless', {
+      engine: DatabaseClusterEngine.AURORA_POSTGRESQL,
+      parameterGroup: ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql10'),
+      vpc: vpc
+    });
   }
+
 }
---
+++ lib/pipeline.ts
@@ -0,0 +1,68 @@
+import { Repository } from "@aws-cdk/aws-codecommit";
+import { Artifact } from "@aws-cdk/aws-codepipeline";
+import { CodeCommitSourceAction } from '@aws-cdk/aws-codepipeline-actions';
+import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
+import { Construct, Stack, Stage, StageProps } from "@aws-cdk/core";
+import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import { CdkStack } from "./cdk-stack";
+
+class MyStage extends Stage {
+    constructor(scope: Construct, id: string, props?: StageProps) {
+        super(scope, id, props);
+
+        const stack = new CdkStack(this, 'mainStack');
+    }
+}
+
+export interface PipelineStackProps {
+    testAccount: string;
+    prodAccount: string;
+};
+
+export class PipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props: StageProps, config:PipelineStackProps) {
+        super(scope, id, props);
+
+        // Create CodePipeline artifact to hold source code from repo
+        const sourceArtifact = new Artifact();
+        // Create CodePipeline artifact to hold synthesized cloud assembly
+        const cloudAssemblyArtifact = new Artifact();
+
+        const repo = new Repository(this, 'repo', { repositoryName: 'assignmentRepo' });
+
+        // Create the CDK pipeline
+        const pipeline = new CdkPipeline(this, 'pipeline', {
+            pipelineName: 'cdkPipeline',
+            cloudAssemblyArtifact: cloudAssemblyArtifact,
+
+            // Checkout source from GitHub
+            sourceAction: new CodeCommitSourceAction({
+                actionName: 'CodeCommit',
+                output: sourceArtifact,
+                repository: repo
+            }),
+            // For synthesize we use the default NPM synth
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+                installCommand: 'npm i -g npm && npm ci',
+                rolePolicyStatements: [
+                    new PolicyStatement({
+                      effect: Effect.ALLOW,
+                      actions: [
+                        "sts:AssumeRole",
+                      ],
+                      resources: [
+                        "arn:aws:iam::*:role/cdk-readOnlyRole"
+                      ]
+                    })
+                  ]
+            })
+        });
+
+        const testStage = pipeline.addApplicationStage(new MyStage(this, 'testStage', { env: { account: config.testAccount, region: 'eu-west-1' } }));
+        testStage.addManualApprovalAction({actionName:'approveToProduction'});
+        pipeline.addApplicationStage(new MyStage(this, 'prodStage', { env: { account: config.prodAccount, region: 'eu-west-1' } }));
+
+    }
+}
